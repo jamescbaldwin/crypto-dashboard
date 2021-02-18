d5bb23afb173ad3c6fd4e0dd888e6fc1bd5be082
James Baldwin <jamescbaldwin@gmail.com>
2021-02-18 13:51:46 -0500

    coin grid added

--- src/Settings/index.js
+++ src/Settings/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import Greeting from "./Greeting";
 import ConfirmButton from "./ConfirmButton";
 import Page from "../Shared/Page";
+import CoinGrid from "./CoinGrid";
 
 export default function() {
     return  <Page name="settings"> 
         <Greeting/> 
         <ConfirmButton/> 
+        <CoinGrid/>
     </Page>
 }
