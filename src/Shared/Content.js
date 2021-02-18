2f4375df907c77681a271e9faa5056f38e99250d
James Baldwin <jamescbaldwin@gmail.com>
2021-02-18 13:45:52 -0500

    loading content added

---
+++ src/Shared/Content.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {AppContext} from "../Application/AppProvider";
+
+export default function(props) {
+    return <AppContext.Consumer>
+        {({coinList}) => {
+            if(!coinList) {
+                return <div> Loading Coins </div>
+            }
+            return <div> {props.children} </div>
+        }}
+    </AppContext.Consumer>
+}
