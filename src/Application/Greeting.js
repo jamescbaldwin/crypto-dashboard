71ab6010ca2f11f4436e76aa1710ba649d1a1c00
James Baldwin <jamescbaldwin@gmail.com>
2021-02-18 00:37:42 -0500

    installed/implemented styled-components

---
+++ src/Application/Greeting.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Greeting() {
+    return <h1>Welcome to Crypto-Tracker</h1>;
+  }
--- src/Application/Index.js
+++ src/Application/Index.js
@@ -1,10 +1,29 @@
+import React, { Component } from "react";
 import "./App.css";
+import Greeting from "./Greeting";
+import styled, {css} from "styled-components";
+
+const MyButton = styled.div`
+  color: green;
+  ${props => props.primary && css`
+  color: palevioletred;
+  `}
+`
+
+const TomatoButton = styled(MyButton)`
+color: tomato;
+border-color: tomato;
+`;
 
 function App() {
   return (
     <div> 
-      Hello
+      <Greeting />
+      <MyButton>Hello</MyButton>
+      <MyButton primary>Hello</MyButton>
+      <TomatoButton>Tomato</TomatoButton>
     </div>
+
   );
 }
 
